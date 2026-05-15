078a524d7f1a409d6616f06771fd5a28669cf27b
backoffice-globalinvest <126775553+backoffice-globalinvest@users.noreply.github.com>
2026-05-15 14:06:58 -0500

    tx centrado

--- app.py
+++ app.py
@@ -873,8 +873,8 @@ with c2:
 
     h(
         '<div class="card">'
-        '<div class="label">TRANSACCIONES ACTUALES</div>'
-        f'<div class="big-number" style="margin-top:10px;">{number_fmt(total_tx_base)}</div>'
+        '<div class="label" style="text-align:center;">TRANSACCIONES ACTUALES</div>'
+        f'<div class="big-number" style="margin-top:10px; text-align:center;">{number_fmt(total_tx_base)}</div>'
         f'<div style="margin-top:14px; color:white; font-size:17px; line-height:1.6;">{lineas_transacciones}</div>'
         '<br><div class="small-text">Escenario base del cliente.</div>'
         '</div>'
